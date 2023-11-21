9a8622a804ca475fe83903b3b74ed548be9cf298
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-11-21 10:14:08 -0500

    11/21/23

--- project/js/index.js
+++ project/js/index.js
@@ -1,5 +1,6 @@
 import '../css/style.css';
 import{booksForSale} from "./sale";
+
  const DOMSelectors = {
     booktitle: document.querySelector("booksForSale.title"),
     bookcover: document.querySelector("booksForSale.img"),
@@ -8,20 +9,20 @@ import{booksForSale} from "./sale";
     changetheme: document.querySelector("#changetheme"),
     instockbooks: document.querySelector("#instockbooks"),
     underten: document.querySelector("#underten"),
-    container: document.querySelector("#container"),
+    container: document.querySelector(".container"),
 };
-booksForSale
-.forEach(el => 
+
+booksForSale.forEach(el => 
 DOMSelectors.container.insertAdjacentHTML(
-    "afterend",
+    "beforeend",
     `<div class = "card">
     <h3 class="booktitle">${el.title}</h3>
     <img src=${el.bookcover}> </img>
     <h4 class="bookauthor">${el.Author}</h4>
     <h5 class="bookprice">${el.Price}</h5>
-    <h5 class="">${el.Price}</h5>
 </div>`
-))
+)) 
+
 /* booksForSale.forEach((book) =>
 book.DOMSelectors.container.insertAdjacentHTML(
     `<div class = "card">
@@ -44,30 +45,34 @@ DOMSelectors.changetheme.addEventListener("click", function (){
 
  function undertenfilter(){
     if(booksForSale.Price < 10){
+        booksForSale.forEach(el => 
         DOMSelectors.container.insertAdjacentHTML(
-            "afterend",
+            "beforeend",
             `<div class = "card">
-            <h3 class="booktitle">${DOMSelectors.booktitle.value}</h3>
-            <img src=${DOMSelectors.inputImage.value}> </img>
-            <h4 class="bookauthor">${DOMSelectors.bookauthor.value}</h4>
-            <h5 class="bookprice">${DOMSelectors.bookprice.value}</h5>
-        </div>`
-       )}
+            <h3 class="booktitle">${el.title}</h3>
+            <img src=${el.bookcover}> </img>
+            <h4 class="bookauthor">${el.Author}</h4>
+            <h5 class="bookprice">${el.Price}</h5>
+            </div>`
+    ))}
 };
 function inStockfilter(){
     if(booksForSale.inStock === true){
+    booksForSale.forEach(el => 
         DOMSelectors.container.insertAdjacentHTML(
-            "afterend",
+            "beforeend",
             `<div class = "card">
-            <h3 class="booktitle">${DOMSelectors.booktitle.value}</h3>
-            <img src=${DOMSelectors.inputImage.value}> </img>
-            <h4 class="bookauthor">${DOMSelectors.bookauthor.value}</h4>
-            <h5 class="bookprice">${DOMSelectors.bookprice.value}</h5>
-        </div>`
-       )}
+            <h3 class="booktitle">${el.title}</h3>
+            <img src=${el.bookcover}> </img>
+            <h4 class="bookauthor">${el.Author}</h4>
+            <h5 class="bookprice">${el.Price}</h5>
+            </div>`
+    ))}
 };
 DOMSelectors.underten.addEventListener("click", function (){
+      /*   document.body.classList.remove(); */
         undertenfilter();
+
     });
 
 DOMSelectors.instockbooks.addEventListener("click", function (){
